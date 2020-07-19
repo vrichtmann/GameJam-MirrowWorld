c295ff608dd1af6fcb08583ffc2bf0b23ac9eabb
vrichtmann <vrichtmann@gmail.com>
2020-07-19 17:51:54 -0300

    Sorting Depth all elements in stage

---
+++ Assets/Scripts/Depth/DepthManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DepthManager : MonoBehaviour
+{
+
+    [SerializeField] private GameObject[] allElements;
+
+    void Update(){
+        for(int i= 0; i < allElements.Length; i++){
+            allElements[i].GetComponentInChildren<SpriteRenderer>().sortingOrder = (int)(allElements[i].transform.position.y * -1) + 10;
+        }
+    }
+}
