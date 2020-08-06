7ca993baee654ff202f8a52a75255bd3bad07125
vrichtmann <vrichtmann@gmail.com>
2020-08-05 23:24:00 -0300

    DG Tween move a key

--- Assets/Scripts/Hud/correctManager.cs
+++ Assets/Scripts/Hud/correctManager.cs
@@ -1,9 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class correctManager : MonoBehaviour{
     [SerializeField]private GameObject[] correctManagers;
+    [SerializeField] private GameObject masterKey;
 
     public int contCorrect = 0;
 
@@ -22,6 +24,10 @@ public class correctManager : MonoBehaviour{
         for (int i = 0; i < contCorrect; i++){
             if(correctManagers[correctManagers.Length - contCorrect]) correctManagers[correctManagers.Length - contCorrect].SetActive(false);
         }
+
+        Debug.Log("SHOW CORRRECT RESPONSE");
+
+        masterKey.GetComponent<Transform>().DOMove(new Vector3(correctManagers[correctManagers.Length - contCorrect].transform.position.x, correctManagers[correctManagers.Length - contCorrect].transform.position.y, -6), 2).SetEase(Ease.InOutCubic).SetDelay(0.4f);
     }
 
 }
